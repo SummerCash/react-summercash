f06585b8468f99d7ab21951896910b955e033085
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-04 12:19:33 -0400

    src: Added drag.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import AuthForm from './AuthForm';
-import * as serviceWorker from './serviceWorker';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
-import { routerReducer } from 'react-router-redux';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
-import Faucet from './Faucet';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import AuthForm from "./AuthForm";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { routerReducer } from "react-router-redux";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Faucet from "./Faucet";
 
 const store = createStore(
-    combineReducers({
-        routing: routerReducer,
-    })
-) // Init store
+  combineReducers({
+    routing: routerReducer
+  })
+); // Init store
 
 const routing = (
-    <Provider store={store}>
-        <Router>
-            <div>
-                <Route exact path="/" component={ App }/>
-                <Route path="/signup" render={ ()=><AuthForm label="Sign Up"/> }/>
-                <Route path="/login" render={ ()=><AuthForm label="Log In"/> }/>
-                <Route path="/faucet" render={ ()=><Faucet/> }/>
-            </div>
-        </Router>
-    </Provider>
-)
+  <Provider store={store}>
+    <Router>
+      <div>
+        <div
+          className="drag"
+          style={{
+            width: "100%",
+            height: "8%",
+            alignContent: "center",
+            alignItems: "center",
+            alignSelf: "center"
+          }}
+        >
+          &nbsp;
+        </div>
+        <Route exact path="/" component={App} />
+        <Route path="/signup" render={() => <AuthForm label="Sign Up" />} />
+        <Route path="/login" render={() => <AuthForm label="Log In" />} />
+        <Route path="/faucet" render={() => <Faucet />} />
+      </div>
+    </Router>
+  </Provider>
+);
 
-
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
