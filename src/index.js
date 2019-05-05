f6ef99cafc96dfcb3e940b0eb25472f35a6621cc
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-05 18:08:08 -0400

    src: Updated service worker register comment.

--- src/index.js
+++ src/index.js
@@ -46,7 +46,5 @@ const routing = (
 
 ReactDOM.render(routing, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Register service worker
 serviceWorker.register();
