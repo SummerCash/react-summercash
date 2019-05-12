d84abb83f4eee466e08bbd7c48d2ec1d6c78651e
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-12 01:38:01 -0400

    src: Updated firebase-messaging-sw.js.

---
+++ src/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/3.4.0/firebase-app.js"); // Import firebase
+importScripts("https://www.gstatic.com/firebasejs/3.4.0/firebase-messaging.js"); // Import firebase
+
+const config = {
+  apiKey: "AIzaSyA0XqseFmaRijRIRmqogPl2jrf7FyuRyeo",
+  authDomain: "summercash-wallet.firebaseapp.com",
+  databaseURL: "https://summercash-wallet.firebaseio.com",
+  projectId: "summercash-wallet",
+  storageBucket: "summercash-wallet.appspot.com",
+  messagingSenderId: "1059498544595",
+  appId: "1:1059498544595:web:44560a2db9a9c19c"
+}; // Set config
+
+firebase.initializeApp(config); // Initialize app
+
+const messaging = firebase.messaging(); // Get messaging
