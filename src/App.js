f225e5a1643b72b266019d7284c55b2e24404b31
Dowland Aiello <mitsukomegumii@gmail.com>
2019-06-03 21:42:08 -0400

    src: Moved it a little more.

--- src/App.js
+++ src/App.js
@@ -306,7 +306,7 @@ class App extends Component {
               Balance: {this.state.balance} SMC
             </Paragraph>
           </Box>
-          <Box marign={{ left: "medium", top: "xsmall" }}>
+          <Box marign={{ left: "medium", top: "small" }}>
             <Down
               onClick={() => this.props.history.push("/settings")}
               cursor="pointer"
